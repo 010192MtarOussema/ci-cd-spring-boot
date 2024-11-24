5168047e36002d2df451b50d68f04704dd572f2f
010192MtarOussema <oussema.mtarr@gmail.com>
2024-11-24 16:53:43 +0100

    test-main

--- src/test/java/com/devops/DemoCiCdApplicationTests.java
+++ src/test/java/com/devops/DemoCiCdApplicationTests.java
@@ -1,13 +1,7 @@
 package com.devops;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoCiCdApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
